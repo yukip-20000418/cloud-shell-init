1628e2e2df9956ff226eea0dff551e4f82f6623b
yukip-chottodake <yukip@chottodake.dev>
2025-05-05 21:58:15 +0900

    git user change

--- cloud-shell-init.sh
+++ cloud-shell-init.sh
@@ -8,6 +8,6 @@ fi
 cp -p ./gcp-yukip.bashrc ~/yukip.bashrc
 cp -p ./gcp-help.txt ~/help.txt
 
-git config --global user.name "yukip"
-git config --global user.email "yukip.20000418@gmail.com"
+git config --global user.name "yukip-chottodake"
+git config --global user.email "yukip@chottodake.dev"
 git config --global credential.helper cache
